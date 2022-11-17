a210b0e1a68449948aec0dfd1d2f6c2f9d2ae5fe
Leo <l30cavalcanti@protonmail.com>
2022-11-17 12:23:29 -0300

    0.0.0.2.1

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,7 +3,7 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
-router.get('/pessoas', PessoaController.lerPessoas)
+router.get('/', PessoaController.lerPessoas)
 router.get('/pessoa/:id', PessoaController.selecionarPessoa)
 router.post('/novapessoa', PessoaController.criarPessoa)
 router.put('/pessoa/:id', PessoaController.atualizarDados)
