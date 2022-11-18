0b95e75d48d7df2bd41948b9e185e6779132991d
Leo <l30cavalcanti@protonmail.com>
2022-11-18 09:08:04 -0300

    0.0.0.4 seeding db with information of the various tables

---
+++ api/seeders/20221118114845-demo-niveis.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+     await queryInterface.bulkInsert('Niveis', [
+			{
+				descr_nivel: 'básico',
+				createdAt: new Date(),
+				updatedAt: new Date()			
+			},
+			{
+				descr_nivel: 'intermediário',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				descr_nivel: 'avançado',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			} 
+    ], {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+
+      await queryInterface.bulkDelete('Niveis', null, {});
+     
+  }
+};
